9f38c50102be0797599599d27f74ec45441c85c5
IvanDragolov <sinkara23@abv.bg>
2018-11-07 11:11:10 +0200

    add ex 1 Define a Class Person from Defining Classes - Exercise / C# OOP Basics

---
+++ Defining Classes/01.Define a Class Person/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Person
+{
+	static void Main(string[] args)
+	{
+		NameAndAge nameAndAge1 = new NameAndAge();
+		nameAndAge1.Name = "Pesho";
+		nameAndAge1.Age = 20;
+		NameAndAge nameAndAge2 = new NameAndAge();
+		nameAndAge2.Name = "Gosho";
+		nameAndAge2.Age = 18;
+		NameAndAge nameAndAge3 = new NameAndAge();
+		nameAndAge3.Name = "Stamat";
+		nameAndAge3.Age = 43;
+
+		string result1 = nameAndAge1.PrintNameAndAge();
+		string result2 = nameAndAge2.PrintNameAndAge();
+		string result3 = nameAndAge3.PrintNameAndAge();
+
+		Console.WriteLine(result1);
+		Console.WriteLine(result2);
+		Console.WriteLine(result3);
+	}
+}
+
---
+++ Defining Classes/01.Define a Class Person/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class NameAndAge
+{
+	int age;
+	string name;
+
+	private NameAndAge(string name, int age)
+	{
+		Name = name;
+		Age = age;
+	}
+
+	public string Name { get => name; set => name = value; }
+
+	public int Age { get => age; set => age = value; }
+
+	string fullNameAndAge;
+
+	public NameAndAge()
+	{
+		fullNameAndAge = age + name;
+	}
+
+	public string PrintNameAndAge()
+	{
+		return $"{this.name} {this.age}";
+	}
+}
+
+
