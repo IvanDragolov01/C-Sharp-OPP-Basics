bb6f0f7c3281677033b09bfe94516e237c459a2b
IvanDragolov <sinkara23@abv.bg>
2018-11-15 09:46:49 +0200

    adding rar files and ex 11 and ex 8

---
+++ Defining Classes/04.Opinion Poll/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace _04.Opinion_Poll
+{
+	class StartUp
+	{
+		static void Main(string[] args)
+		{
+			var people = GetPeople();
+			PrintPeopleOlderThanthirthy(people);
+		}
+
+		private static void PrintPeopleOlderThanthirthy(HashSet<Person> people)
+		{
+			Console.WriteLine(string.Join(Environment.NewLine, people
+				.Where(p => p.Age > 30)
+				.OrderBy(p => p.Name)
+				.Select(p => $"{p.Name} - {p.Age}")));
+		}
+
+		private static HashSet<Person> GetPeople()
+		{
+			var people = new HashSet<Person>();
+			var numberOfPeople = int.Parse(Console.ReadLine());
+
+			while (numberOfPeople > 0)
+			{
+				var personData = Console.ReadLine().Split();
+				people.Add(new Person(personData[0], int.Parse(personData[1])));
+				numberOfPeople--;
+			}
+
+			return people;
+		}
+	}
+}
