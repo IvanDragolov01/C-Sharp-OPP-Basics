a7c7f13cf4b3b3f6d483284fb8702ac65f08b431
IvanDragolov <sinkara23@abv.bg>
2018-11-26 11:17:42 +0200

    modifie ex 1 and ex 2

--- Encapsulation/02.ClassBoxDataValidation/Program.cs
+++ Encapsulation/02.ClassBoxDataValidation/Program.cs
@@ -4,7 +4,7 @@ namespace _02.ClassBoxDataValidation
 {
 	class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
 			double length = double.Parse(Console.ReadLine());
 			double width = double.Parse(Console.ReadLine());
@@ -14,9 +14,10 @@ namespace _02.ClassBoxDataValidation
 			{
 				Box box = new Box(length, width, height);
 
-				double surfaceArea = Box.SurfaceArea(length, width, height);
-				double lateralSurfaceArea = Box.LateralSurfaceArea(length, width, height);
-				double volume = Box.Volume(length, width, height);
+				double surfaceArea = box.SurfaceArea();
+				double lateralSurfaceArea = box.LateralSurfaceArea();
+				double volume = box.Volume();
+
 				Console.WriteLine($"Surface Area - {surfaceArea:F2}");
 				Console.WriteLine($"Lateral Surface Area - {lateralSurfaceArea:F2}");
 				Console.WriteLine($"Volume - {volume:F2}");
